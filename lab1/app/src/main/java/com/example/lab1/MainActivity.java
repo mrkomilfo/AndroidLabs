9d30e170a4c3c013d60f7ed50057b7c5f230ae73
mrkomilfo <43483871+mrkomilfo@users.noreply.github.com>
2019-09-24 03:59:59 +0300

    version and imei display added

--- lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showVersion();
+        showImei();
+    }
+
+    protected void showVersion() {
+        TextView version = (TextView) findViewById(R.id.versionLabel);
+        version.setText(BuildConfig.VERSION_NAME);
+    }
+
+    protected void showImei() {
+        TextView imei = (TextView) findViewById(R.id.imeiLabel);
+
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)
+        {
+            TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            String deviceId = tm.getDeviceId();
+            imei.setText(deviceId);
+        }
+        else
+        {
+            imei.setText("Permission denied");
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE))
+            {
+                Toast.makeText(this,
+                        "This permission is needed to show your IMEI.",
+                        Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 2);
+        }
+    }
+
+    @Override
+    public  void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (requestCode == 2)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                showImei();
+            }
+            else
+            {
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
